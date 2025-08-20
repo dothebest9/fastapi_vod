b30b7516d271c47104bb478290483b7ba08eceec
dothebest9 <smrmtjsrn11@naver.com>
2025-08-20 15:01:34 +0900

    test add meeting create spec

--- main.py
+++ main.py
@@ -4,41 +4,6 @@ from typing import Optional
 import uvicorn
 from pydantic import BaseModel, Field
 
-
-# 성별을 위한 Enum 정의
-class Gender(str, Enum):
-    male = "male"
-    female = "female"
-
-
-# 유저 생성 요청 데이터의 스키마에 age와 gender 필드 추가
-class UserCreateRequest(BaseModel):
-    username: str
-    email: str
-    age: int
-    gender: Gender
-
-
-
-class UserUpdateRequest(BaseModel):
-    username: Optional[str] = None
-    email: Optional[str] = None
-
-
-class UserResponse(BaseModel):
-    id: int
-    username: str
-    email: str
-
-
-class UserSearchParams(BaseModel):
-    model_config = {"extra": "forbid"}
-
-    username: Optional[str] = None
-    age: Optional[int] = Field(None, gt=0)
-    gender: Optional[Gender] = None
-
-
 if __name__ == "__main__":
     import uvicorn
 
